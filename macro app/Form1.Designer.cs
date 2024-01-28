52567bfab238acc7fe4c267c2f1dc77dae240f1d
dylan rampersad <67800304+rdylan1999@users.noreply.github.com>
2024-01-28 20:06:05 +0100

    Initial commit

---
+++ macro app/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace macro_app
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AccessibleDescription = "";
+            this.button1.AccessibleName = "";
+            this.button1.Location = new System.Drawing.Point(161, 92);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "start\r\n\r\n";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(413, 242);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Macro King";
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button button1;
+
+        #endregion
+    }
+}
---
+++ macro app/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace macro_app
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        public static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, UIntPtr dwExtraInfo);
+
+        const int KEYEVENTF_EXTENDEDKEY = 0x0001;
+        const int KEYEVENTF_KEYUP = 0x0002;
+        
+        [DllImport("user32.dll")]
+        public static extern bool GetCursorPos(out POINT lpPoint);
+
+        [DllImport("user32.dll")]
+        public static extern short GetAsyncKeyState(int vKey);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int X;
+            public int Y;
+        }
+        
+        static void SimulateKeyPress(byte keyCode)
+        {
+            keybd_event(keyCode, 0, KEYEVENTF_EXTENDEDKEY, UIntPtr.Zero);
+        }
+
+        public static bool isEnabled = false;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (button1.Text.Equals("start"))
+            {
+                button1.Text = "stop";
+                isEnabled = true;
+                input_Listener();
+                
+                MessageBox.Show("Macro is running...");
+                WindowState = FormWindowState.Minimized;
+            }
+            
+            else
+            {
+                button1.Text = "start";
+                isEnabled = false;
+            }
+        }
+
+        static async void input_Listener()
+        {
+            POINT point;
+
+            while (isEnabled)
+            {
+                // Check if the left mouse button is clicked
+                if ((GetAsyncKeyState(0x02) & 0x8000) != 0)
+                {
+                    // Get the current cursor position
+                    GetCursorPos(out point);
+                    Console.WriteLine("Mouse clicked at X: {0}, Y: {1}", point.X, point.Y);
+
+                    // Simulate pressing keys '1' through '7' simultaneously
+                    SimulateKeyPress(0x31); // '1'
+                    SimulateKeyPress(0x32); // '2'
+                    SimulateKeyPress(0x33); // '3'
+                    SimulateKeyPress(0x34); // '4'
+                    SimulateKeyPress(0x35); // '5'
+                    SimulateKeyPress(0x36); // '6'
+                    SimulateKeyPress(0x37); // '7'
+                }
+
+                // change the loop speed, otherwise one click might invoke the function multiple times at once
+                await Task.Delay(75);
+            }
+        }
+        
+    }
+
+}
